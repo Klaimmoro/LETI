cee0a00f31773f6d758ae22581b5cb32e894ac0a
Zack <113541780+Klaimmoro@users.noreply.github.com>
2024-05-08 20:30:47 +0300

    Add files via upload

---
+++ CardScanner/CardScanner/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace CardScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(500, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "СТАРТ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1095, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите логин и пароль для входа в систему, и нажмите кнопку старт";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(63, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 114);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "СТОП";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(57, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(358, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(727, 483);
+            this.panel1.TabIndex = 5;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(63, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 114);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "СБРОС";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(746, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(746, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(746, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Логин";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(746, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Пароль";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(500, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BarCodeScan);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CardScanner/CardScanner/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CardScanner
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con;
+        public bool stopwhile = false;
+        public string BarCode = "";
+        int x = 0; int y = 0;
+        bool connection = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void connectionSQLWindows()
+        {
+            string connstring = "Data Source = KLAIMMOR;Initial Catalog = CardScan;Integrated Security = true";
+            con = new SqlConnection(connstring);
+            
+        }
+
+        private void connectionSQLUser()
+        {
+
+            string connstring = "Data Source = KLAIMMOR;Initial Catalog = CardScan;Persist Security Info = true;User ID=" + textBox1.Text + ";Password=" + textBox2.Text + ";";
+            con = new SqlConnection(connstring);
+
+            try
+            {
+                con.Open();
+                label5.Visible = false;
+                connection = true;
+            }
+            catch
+            {
+                label5.Text = "Ошибка соединения с базой данных, проверьте правильность пароля и логина!";
+                connection = false;
+            }
+            //Application.DoEvents();
+            con.Close();
+        }
+
+        private void BarCodeScan(object sender, KeyEventArgs e)
+        {
+            if (connection)
+            {
+                if (con != null)
+                {
+                    if ('A' - 1 < (char)e.KeyCode && (char)e.KeyCode < 'Z')
+                    {
+                        BarCode += (char)e.KeyCode;
+                    }
+                    else if ('0' - 1 < (char)e.KeyCode && (char)e.KeyCode < '9' + 1)
+                    {
+                        BarCode += (char)e.KeyCode;
+                    }
+                    Thread.Sleep(10);
+                    if (BarCode.Length == 2)
+                    {
+                        cardScan();
+                    }
+                }
+            }
+        }
+
+
+        private void cardScan()
+        { 
+            string card = "";
+            switch (BarCode[0])
+            {
+                case 'S':
+                    card += "Spades";
+                    break;
+                case 'H':
+                    card += "Hearts";
+                    break;
+                case 'D':
+                    card += "Diamonds";
+                    break;
+                case 'C':
+                    card += "Clubs";
+                    break;
+                default:
+                    break;
+            }
+            card += " ";
+            switch (BarCode[1])
+            {
+                case 'A':
+                    card += "Ace";
+                    break;
+                case 'K':
+                    card += "King";
+                    break;
+                case 'Q':
+                    card += "Queen";
+                    break;
+                case 'J':
+                    card += "Jack";
+                    break;
+                default:
+                    card += BarCode[1];
+                    break;
+            }
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = Image.FromFile("C:\\Users\\klaim\\Downloads\\" + BarCode[0] + BarCode[1] + ".png");
+            pictureBox.Location = new Point(x, y);
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            x += 100;
+            if (x + 100 > 720)
+            {
+                x = 0;
+                y += 150;
+
+            }
+            panel1.Controls.Add(pictureBox);
+            mainFunc(card);
+        }
+
+
+        private void mainFunc(string card)
+        {
+            bool openCon = false;
+            if (stopwhile == false)
+            {
+                if (openCon == false)
+                {
+                    con.Open();
+                    openCon = true;
+                }
+                if (BarCode.Length == 2)
+                {
+                    panel1.Show();
+                    label2.Text = card;
+                    string query = "DECLARE @currentTime TIME(7);\r\n    " +
+                    "SET @currentTime = CONVERT(TIME(7), GETDATE());\r\n" +
+                    "EXECUTE InsertCard 1," + card.Substring(0, card.IndexOf(" ")) + "," + card.Substring(card.IndexOf(" ")) + ", 1, @currentTime ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    BarCode = "";
+                    card = "";
+
+                }
+                con.Close();
+                openCon = false;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectionSQLUser();
+            //connectionSQLWindows();
+            if (connection)
+            {
+                label1.Text = "Программа готова для считывания карт";
+                button1.Visible = false;
+                button2.Visible = true;
+                button3.Visible = true;
+                panel1.Visible = true;
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopwhile = true;
+            Application.Exit();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            x = 0;
+            y = 0;
+            stopwhile = false;
+            
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
